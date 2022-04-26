e3218f32091d9cfc81bdf0e62275902cbf0c78f1
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2022-04-25 21:14:18 -0300

    Example of antiderivatives with plot

---
+++ antiderivatives.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from sympy import Symbol, plot
+from helpers import move_sympy_plot_to_axes
+
+
+x = Symbol('x')
+
+fig, ax = plt.subplots(figsize=(8, 6))
+
+for i in range(-2, 3):
+    expr = x**2 + i
+    text_label = rf"$x^2 {i:+g}$"
+    if i == 0:
+        text_label = r"$x^2$"
+    p = plot(expr, (x, -3, 3), ylim=(-2.5, 4), label=text_label, show=False)
+    move_sympy_plot_to_axes(p, ax)
+
+ax.legend(loc='best')
+plt.show()
