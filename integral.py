153b5c48c952fb31951a3d8e0450946542795c71
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2022-04-26 18:52:19 -0300

    Riemann calcs methods. Readability counts

--- integral.py
+++ integral.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from sympy import Symbol, plot, lambdify, integrate, N
 from helpers import move_sympy_plot_to_axes
 import numpy as np
+from collections import namedtuple
 
 
 class Integral:
@@ -60,6 +61,28 @@ class Integral:
         # ax.legend(loc='best')
         plt.show()
 
+    def riemann_x_y(self):
+        x_values = np.linspace(*self.interval, self.bars+1)
+        f = lambdify(self.x, self.sympy_expression, 'numpy')
+        return x_values, f
+
+    def riemann_calculations(self):
+        x_values, f = self.riemann_x_y()
+
+        x_left = x_values[:-1]
+        y_left = f(x_values)[:-1]
+        x_right = x_values[1:]
+        y_right = f(x_values)[1:]
+        x_mid = (x_left + x_right)/2
+        y_mid = f(x_mid)
+        bar_width = (self.interval[1] - self.interval[0])/self.bars
+
+        Riemann = namedtuple('Riemann',
+                             ['x_left', 'y_left', 'x_right', 'y_right',
+                              'x_mid', 'y_mid', 'bar_width'])
+        return Riemann(x_left, y_left, x_right, y_right,
+                       x_mid, y_mid, bar_width)
+
     def riemann_plot(self):
         # TODO update the docs
         fig, arr = plt.subplots(nrows=1, ncols=3, figsize=(15, 5),
@@ -69,50 +92,35 @@ class Integral:
 
         for ax in arr:
             move_sympy_plot_to_axes(p, ax)
+        
+        Riemann = self.riemann_calculations()
 
-        x_values = np.linspace(*self.interval, self.bars+1)
-        f = lambdify(self.x, self.sympy_expression, 'numpy')
-
-        x_left = x_values[:-1]
-        y_left = f(x_values)[:-1]
-        x_right = x_values[1:]
-        y_right = f(x_values)[1:]
-        x_mid = (x_left + x_right)/2
-        y_mid = f(x_mid)
-
-        arr[0].scatter(x_left, y_left, s=10)
-        arr[0].bar(x_left, y_left,
-                   width=(self.interval[1]-self.interval[0])/self.bars,
+        arr[0].scatter(Riemann.x_left, Riemann.y_left, s=10)
+        arr[0].bar(Riemann.x_left, Riemann.y_left,
+                   width=Riemann.bar_width,
                    align='edge', alpha=0.2, edgecolor='orange')
-        arr[1].scatter(x_mid, y_mid, s=10)
-        arr[1].bar(x_mid, y_mid,
-                   width=(self.interval[1]-self.interval[0])/self.bars,
+        arr[1].scatter(Riemann.x_mid, Riemann.y_mid, s=10)
+        arr[1].bar(Riemann.x_mid, Riemann.y_mid,
+                   width=Riemann.bar_width,
                    align='center', alpha=0.2, edgecolor='orange')
-        arr[2].scatter(x_right, y_right, s=10)
-        arr[2].bar(x_right, y_right,
-                   width=-(self.interval[1]-self.interval[0])/self.bars,
+        arr[2].scatter(Riemann.x_right, Riemann.y_right, s=10)
+        arr[2].bar(Riemann.x_right, Riemann.y_right,
+                   width=-Riemann.bar_width,
                    align='edge', alpha=0.2, edgecolor='orange')
 
         plt.show()
 
     def riemann_sum(self, method='midpoint'):
         # TODO update the docs
-
-        x_values = np.linspace(*self.interval, self.bars+1)
-        f = lambdify(self.x, self.sympy_expression, 'numpy')
-
-        x_left = x_values[:-1]
-        x_right = x_values[1:]
-        x_mid = (x_left + x_right)/2
-
-        width = (self.interval[1] - self.interval[0])/self.bars
+        _, f = self.riemann_x_y()
+        Riemann = self.riemann_calculations()
 
         if method == 'left':
-            return np.sum(f(x_left)*width)
+            return np.sum(f(Riemann.x_left)*Riemann.bar_width)
         elif method == 'right':
-            return np.sum(f(x_right)*width)
+            return np.sum(f(Riemann.x_right)*Riemann.bar_width)
         elif method == 'midpoint':
-            return np.sum(f(x_mid)*width)
+            return np.sum(f(Riemann.x_mid)*Riemann.bar_width)
         else:
             raise ValueError("Method must be 'left', 'right' or 'midpoint'.")
 
