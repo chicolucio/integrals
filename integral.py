05c2c27e97994104932b6f9f2728e72134bd03c6
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2022-04-26 22:37:10 -0300

    Plots with annotations and titles

--- integral.py
+++ integral.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from sympy import Symbol, plot, lambdify, integrate, N
+from sympy import Symbol, plot, lambdify, integrate, N, latex
 from helpers import move_sympy_plot_to_axes
 import numpy as np
 from collections import namedtuple
@@ -43,6 +43,13 @@ class Integral:
         else:
             self._bars = value
 
+    @property
+    def _latex_strings(self):
+        Latex = namedtuple('Latex', ('expr', 'lower_lim', 'upper_lim'))
+        return Latex(f'${latex(self.sympy_expression)}$',
+                     f'${latex(self.interval[0])}$',
+                     f'${latex(self.interval[1])}$')
+
     def definite_integral_fill_plot(self, nb_points=100, figsize=(8, 6)):
         # TODO update the docs
 
@@ -50,8 +57,10 @@ class Integral:
         # of a figsize
 
         fig, ax = plt.subplots(figsize=figsize)
+        Latex = self._latex_strings
 
-        p = plot(self.sympy_expression, (self.x, *self.domain), show=False)
+        p = plot(self.sympy_expression, (self.x, *self.domain), show=False,
+                 label=Latex.expr)
         move_sympy_plot_to_axes(p, ax)
 
         try:
@@ -62,7 +71,12 @@ class Integral:
         f = lambdify(self.x, self.sympy_expression, 'numpy')
         ax.fill_between(x_fill, f(x_fill))
 
-        # ax.legend(loc='best')
+        main_title = f'Plot for {Latex.expr}\n'
+        area_title = f'Area from {Latex.lower_lim} to {Latex.upper_lim}'
+        title = main_title + area_title
+
+        # ax.legend(loc='best', fontsize=14)
+        plt.title(title, fontsize=16)
         plt.show()
 
     @property
@@ -95,8 +109,9 @@ class Integral:
 
     def riemann_plot(self):
         # TODO update the docs
+        Latex = self._latex_strings
         fig, arr = plt.subplots(nrows=1, ncols=3, figsize=(15, 5),
-                                constrained_layout=True, facecolor=(1, 1, 1))
+                                facecolor=(1, 1, 1))
 
         p = plot(self.sympy_expression, (self.x, *self.domain), show=False)
 
@@ -109,15 +124,70 @@ class Integral:
         arr[0].bar(Riemann.x_left, Riemann.y_left,
                    width=Riemann.bar_width,
                    align='edge', alpha=0.2, edgecolor='orange')
+        arr[0].set_title(f'Left Riemann Sum - N = {self.bars}')
+        arr[0].annotate(f'Exact value: {self.exact_integral_value()}',
+                        xy=(0.5, 0.), xytext=(0, 30),
+                        xycoords=('axes fraction', 'figure fraction'),
+                        textcoords='offset points',
+                        ha='center', va='bottom', size=12)
+        arr[0].annotate(f'Approximate value: {self.riemann_sum.left}',
+                        xy=(0.5, 0.), xytext=(0, 15),
+                        xycoords=('axes fraction', 'subfigure fraction'),
+                        textcoords='offset points',
+                        ha='center', va='bottom', size=12)
+        arr[0].annotate(f'Error: {self.riemann_errors.left}',
+                        xy=(0.5, 0.), xytext=(0, 0),
+                        xycoords=('axes fraction', 'figure fraction'),
+                        textcoords='offset points',
+                        ha='center', va='bottom', size=12)
+
         arr[1].scatter(Riemann.x_mid, Riemann.y_mid, s=10)
         arr[1].bar(Riemann.x_mid, Riemann.y_mid,
                    width=Riemann.bar_width,
                    align='center', alpha=0.2, edgecolor='orange')
+        arr[1].set_title(f'Midpoint Riemann Sum - N = {self.bars}')
+        arr[1].annotate(f'Exact value: {self.exact_integral_value()}',
+                        xy=(0.5, 0.), xytext=(0, 30),
+                        xycoords=('axes fraction', 'figure fraction'),
+                        textcoords='offset points',
+                        ha='center', va='bottom', size=12)
+        arr[1].annotate(f'Approximate value: {self.riemann_sum.mid}',
+                        xy=(0.5, 0.), xytext=(0, 15),
+                        xycoords=('axes fraction', 'subfigure fraction'),
+                        textcoords='offset points',
+                        ha='center', va='bottom', size=12)
+        arr[1].annotate(f'Error: {self.riemann_errors.mid}',
+                        xy=(0.5, 0.), xytext=(0, 0),
+                        xycoords=('axes fraction', 'figure fraction'),
+                        textcoords='offset points',
+                        ha='center', va='bottom', size=12)
+
         arr[2].scatter(Riemann.x_right, Riemann.y_right, s=10)
         arr[2].bar(Riemann.x_right, Riemann.y_right,
                    width=-Riemann.bar_width,
                    align='edge', alpha=0.2, edgecolor='orange')
+        arr[2].set_title(f'Right Riemann Sum - N = {self.bars}')
+        arr[2].annotate(f'Exact value: {self.exact_integral_value()}',
+                        xy=(0.5, 0.), xytext=(0, 30),
+                        xycoords=('axes fraction', 'figure fraction'),
+                        textcoords='offset points',
+                        ha='center', va='bottom', size=12)
+        arr[2].annotate(f'Approximate value: {self.riemann_sum.right}',
+                        xy=(0.5, 0.), xytext=(0, 15),
+                        xycoords=('axes fraction', 'subfigure fraction'),
+                        textcoords='offset points',
+                        ha='center', va='bottom', size=12)
+        arr[2].annotate(f'Error: {self.riemann_errors.right}',
+                        xy=(0.5, 0.), xytext=(0, 0),
+                        xycoords=('axes fraction', 'figure fraction'),
+                        textcoords='offset points',
+                        ha='center', va='bottom', size=12)
 
+        main_title = f'Plot for {Latex.expr}\n'
+        area_title = f'Area from {Latex.lower_lim} to {Latex.upper_lim}'
+        title = main_title + area_title
+        fig.suptitle(title, fontsize=18)
+        fig.tight_layout(rect=[0, 0.07, 1, 1.0])
         plt.show()
 
     @property
